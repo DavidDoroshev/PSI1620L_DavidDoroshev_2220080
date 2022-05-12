99e35f0c5f9b5be58a57979c6912ada7ceade94f
DavidDoroshev <104576635+DavidDoroshev@users.noreply.github.com>
2022-05-12 22:09:14 +0100

    Tentando fazer o comobox com o codigo postal

--- Trabalho_marcacoes_/Trabalho_marcacoes_/Form2.cs
+++ Trabalho_marcacoes_/Trabalho_marcacoes_/Form2.cs
@@ -39,6 +39,7 @@ namespace Trabalho_marcacoes_
 
         private void guardar_cliente_Click(object sender, EventArgs e)
         {
+            ligarDB.Open();
             SqlCommand command = new SqlCommand();
 
             command.Connection = ligarDB;
@@ -46,11 +47,29 @@ namespace Trabalho_marcacoes_
             command.CommandText = "SELECT * FROM filmes WHERE Nome = @nome";
             command.Parameters.Add("@nome", System.Data.SqlDbType.VarChar).Value = nome_guardar;
             command.Parameters.Add("@password", System.Data.SqlDbType.VarChar).Value = password_guardar;
-            command.Parameters.Add("@codigo_postal_cliente", System.Data.SqlDbType.VarChar).Value = password_guardar;
-            ligarDB.Open();
+            command.Parameters.Add("@codigo_postal_cliente", System.Data.SqlDbType.VarChar).Value = comboBox1;
+            
 
             SqlDataReader commandReader = command.ExecuteReader();
             commandReader.Read();
+
+            try
+            {
+           
+                SqlCommand postal = new SqlCommand("select (distrito_codigo) from codigo_postal ");
+                SqlDataReader reader;
+                reader = postal.ExecuteReader();
+                DataTable dtc = new DataTable();
+                dtc.Columns.Add("distrito_codigo", typeof(string));
+                dtc.Load(reader);
+                comboBox1.ValueMember = "distrito_codigo";
+                comboBox1.DataSource = dtc;
+               
+            }
+            catch (Exception)
+            {
+
+            }
 
         }
 
@@ -63,6 +82,10 @@ namespace Trabalho_marcacoes_
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+
+
+
+
 
         }
     }
