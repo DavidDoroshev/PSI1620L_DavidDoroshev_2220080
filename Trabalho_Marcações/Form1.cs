6a97948970c61b449204e15c7a4aa3c5c8b0c7f7
DavidDoroshev <104576635+DavidDoroshev@users.noreply.github.com>
2022-05-12 13:50:42 +0100

    Trabalho_teste

---
+++ Trabalho_Marcações/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabalho_Marcações
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Hide();
+            Criar_Conta_CLiente criar_conta = new Criar_Conta_CLiente();
+            criar_conta.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
